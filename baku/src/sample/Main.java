673fc5b7e23ebbcffbaf825727e29f4b3d714e74
Matlab1996 <matlab.96@mail.ru>
2017-05-31 19:04:48 +0400

    Add project

---
+++ baku/src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Baku");
+        primaryStage.setScene(new Scene(root, 700, 550));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        launch(args);
+    }
+}
