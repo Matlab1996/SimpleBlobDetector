b0caea6e82ab54b49434ab6ec9bbc2e6bf3cee9c
Matlab1996 <matlab.96@mail.ru>
2018-07-14 14:55:31 +0400

    CanvasPanel #2

--- src/Webcam/CanvasPanel.java
+++ src/Webcam/CanvasPanel.java
@@ -11,20 +11,26 @@ public class CanvasPanel extends JFrame{
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(640, 720);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
     }
     @Override
     public void paint(Graphics g) {
-    	g.setColor(Color.WHITE);
-    	g.fillRect(0, 0, getWidth(), getHeight());
     	g.setColor(Color.BLACK);
+    	g.fillRect(0, 0, getWidth(), getHeight());
+    	g.setColor(Color.GREEN);
+    	//g.setFont(new Font("Times New Roman", Font.BOLD, 44));
     	
-    	for(int x = 0; x <= getWidth(); x+=getWidth()/2) {
-    		g.drawLine(x, 0, x, getHeight());
+    	for(int x = 0; x <= getWidth(); x+=(getWidth()-1)/2) {
+    		for(int y = 0; y <= getHeight(); y+=(getHeight()-1)/2) {
+	    		//g.drawString("+", x-11, y+15);
+	    		g.drawLine(x, y-10, x, y+10);
+	    		g.drawLine(x-10, y, x+10, y);
+    		}
     	}
-    	
-    	for(int y = 0; y <= getHeight(); y+=getHeight()/2) {
-			g.drawLine(0, y, getWidth(), y);
-		}
-    }
+    } 
+    
+    //public static void main(String args[]) {
+    //	new CanvasPanel();
+    //}
     
 }
